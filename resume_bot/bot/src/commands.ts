f4701e195fba26340b2df83f5ed60ec01e6afb2a
hariton <luckydjone@inbox.ru>
2025-09-17 14:35:38 +0300

    add weather

--- resume_bot/bot/src/commands.ts
+++ resume_bot/bot/src/commands.ts
@@ -7,7 +7,8 @@ export const commandsBot = (bot: Telegraf) => {
         reply_markup: {
             keyboard: [
                 [{ text: "Резюме" }, { text: "GitHub" }],
-                [{text: "MiniApp"}, { text: "Contacts"}]
+                [{text: "MiniApp"}, { text: "Contacts"}],
+                [{text: "Погода в Москве"}]
             ],
             resize_keyboard: true
         }
@@ -31,6 +32,31 @@ export const commandsBot = (bot: Telegraf) => {
 })
     bot.hears("Contacts", (ctx) => {
     ctx.reply("Email: luckydjone@inbox.ru\nTelegram: @Haritonov_Evgenuy")
+})
+        bot.hears("Погода в Москве", async (ctx) => {
+            try {
+                const apiKey = process.env.WEATHER_API_KEY;
+                const city = "Moscow";
+                const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`;
+                const res = await fetch(url)
+                const data = await res.json()
+                console.log(data)
+
+                if (data.cod !== 200) {
+                    return ctx.reply("Не удалось получить погоду")
+                }
+                    const temp = data.main.temp;
+                    const desc = data.weather[0].description;
+                    const feels = data.main.feels_like;
+                    const humidity = data.main.humidity;
+
+                      ctx.reply(
+                        `Погода в Москве:\n🌡 Температура: ${temp}°C\n🤔 Ощущается как: ${feels}°C\n💧 Влажность: ${humidity}%\n🌥 ${desc}`
+                      );
+            } catch(err) {
+                console.error(err);
+                ctx.reply("Произошла ошибка")
+            }
 })
 
 }
